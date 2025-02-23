11762403ce3daeee94f4008677bf6b6096bf2b98
Ted Charles Brown <52015952+Tedcharlesbrown@users.noreply.github.com>
2025-02-23 10:55:17 -0600

    Update variables.js

--- variables.js
+++ variables.js
@@ -14,23 +14,13 @@ export async function request_api(self) {
 	// Get annotations for all tracks
 	try {
 		if (self.transport_tracks && self.transport_tracks.result) {
-			// Initialize array to store all annotation results
 			self.transport_annotations = { result: [] }
 			
-			self.log('debug', 'Fetching annotations for tracks: ' + JSON.stringify(self.transport_tracks.result))
-			
-			// Fetch annotations for each track
 			for (const track of self.transport_tracks.result) {
 				if (track.uid) {
 					try {
-						const annotationUrl = `http://${self.config.ipaddress}/api/session/transport/annotations?uid=${track.uid}`
-						self.log('debug', `Fetching annotations from: ${annotationUrl}`)
-						
-						const trackAnnotations = await getRequest(annotationUrl)
-						self.log('debug', `Received annotations for track ${track.name}: ${JSON.stringify(trackAnnotations)}`)
-						
+						const trackAnnotations = await getRequest(`http://${self.config.ipaddress}/api/session/transport/annotations?uid=${track.uid}`)
 						if (trackAnnotations && trackAnnotations.result) {
-							// Add track name to the annotation result for reference
 							trackAnnotations.result.name = track.name
 							self.transport_annotations.result.push(trackAnnotations.result)
 						}
@@ -39,15 +29,12 @@ export async function request_api(self) {
 					}
 				}
 			}
-			
-			self.log('debug', 'Final annotations object: ' + JSON.stringify(self.transport_annotations))
 		}
 	} catch (error) {
 		self.log('debug', `Failed to get annotations: ${error.message}`)
 		self.transport_annotations = { result: [] }
 	}
 	
-	// Update transport choices after getting new data
 	updateTransportChoices(self)
 }
 
@@ -55,27 +42,16 @@ export async function defineVariables(self) {
 	await request_api(self)
 
 	try {
-		// Fetch variable definitions
 		const transportVariableDefinitions = await fetchTransportVariableDefinitions(self, [self.transport_activetransport, self.transport_tracks]);
 		const statusVariableDefinitions = await fetchStatusVariableDefinitions(self, [self.status_health, self.status_project]);
 		const annotationVariableDefinitions = await fetchAnnotationVariableDefinitions(self, self.transport_annotations);
 
-		// Debug log before merging
-		self.log('debug', 'Transport Variables: ' + JSON.stringify(transportVariableDefinitions));
-		self.log('debug', 'Status Variables: ' + JSON.stringify(statusVariableDefinitions));
-		self.log('debug', 'Annotation Variables: ' + JSON.stringify(annotationVariableDefinitions));
-
-		// Merge all variable definitions
 		const allVariableDefinitions = [
 			...transportVariableDefinitions,
 			...statusVariableDefinitions,
 			...annotationVariableDefinitions
 		];
 
-		// Debug log final definitions
-		self.log('debug', 'Setting variable definitions: ' + JSON.stringify(allVariableDefinitions));
-
-		// Set the merged variable definitions
 		self.setVariableDefinitions(allVariableDefinitions);
 	} catch (err) {
 		self.log('error', `Failed to fetch and set variable definitions: ${err.message}`);
@@ -84,27 +60,16 @@ export async function defineVariables(self) {
 
 export async function updateVariables(self) {
 	try {
-		// Fetch variable values
 		const transportVariableValues = await fetchTransportVariableValues(self, [self.transport_activetransport, self.transport_tracks]);
 		const statusVariableValues = await fetchStatusVariableValues(self, [self.status_health, self.status_project]);
 		const annotationVariableValues = await fetchAnnotationVariableValues(self, self.transport_annotations);
 
-		// Debug log before merging
-		self.log('debug', 'Transport Values: ' + JSON.stringify(transportVariableValues));
-		self.log('debug', 'Status Values: ' + JSON.stringify(statusVariableValues));
-		self.log('debug', 'Annotation Values: ' + JSON.stringify(annotationVariableValues));
-
-		// Merge all variable values
 		const allVariableValues = {
 			...transportVariableValues,
 			...statusVariableValues,
 			...annotationVariableValues
 		};
 
-		// Debug log final values
-		self.log('debug', 'Setting variable values: ' + JSON.stringify(allVariableValues));
-
-		// Set the merged variable values
 		self.setVariableValues(allVariableValues);
 	} catch (error) {
 		self.log('error', `Failed to update variables: ${error.message}`);
