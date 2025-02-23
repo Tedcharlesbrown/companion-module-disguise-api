fc3b1411d5a9b88de75fb0b2533d00ff947786d2
Ted Charles Brown <52015952+Tedcharlesbrown@users.noreply.github.com>
2025-02-23 10:29:08 -0600

    Update annotationVariables.js

--- api/session/transport/annotationVariables.js
+++ api/session/transport/annotationVariables.js
@@ -1,39 +1,45 @@
 export async function fetchAnnotationVariableDefinitions(self, data) {
     const variableDefinitions = []
 
-    // Handle single annotation result
-    const annotations = data.result
-    if (!annotations) return variableDefinitions
-
-    // Get transport name from the result and sanitize it for use in variable names
-    const trackName = annotations.name ? annotations.name.replace(/\s+/g, '_') : 'unknown'
-
-    // Add variables for notes
-    if (annotations.annotations && annotations.annotations.notes) {
-        annotations.annotations.notes.forEach((note, index) => {
-            variableDefinitions.push(
-                { 
-                    variableId: `track_${trackName}_note${index + 1}_text`, 
-                    name: `Track ${trackName}: Note ${index + 1} Text` 
-                }
-            )
-        })
+    // Handle array of annotation results
+    if (!Array.isArray(data)) {
+        data = [data]
     }
 
-    // Add variables for tags
-    if (annotations.annotations && annotations.annotations.tags) {
-        annotations.annotations.tags.forEach((tag, index) => {
-            variableDefinitions.push(
-                { 
-                    variableId: `track_${trackName}_tag${index + 1}_type`, 
-                    name: `Track ${trackName}: Tag ${index + 1} Type` 
-                },
-                { 
-                    variableId: `track_${trackName}_tag${index + 1}_value`, 
-                    name: `Track ${trackName}: Tag ${index + 1} Value` 
-                }
-            )
-        })
+    for (const annotationData of data) {
+        const annotations = annotationData.result
+        if (!annotations) continue
+
+        // Get transport name from the result and sanitize it for use in variable names
+        const transportName = annotations.name ? annotations.name.replace(/\s+/g, '_') : 'unknown'
+
+        // Add variables for notes
+        if (annotations.annotations && annotations.annotations.notes) {
+            annotations.annotations.notes.forEach((note, index) => {
+                variableDefinitions.push(
+                    { 
+                        variableId: `annotation_track_${transportName}_note${index + 1}_text`, 
+                        name: `Track ${annotations.name}: Note ${index + 1} Text` 
+                    }
+                )
+            })
+        }
+
+        // Add variables for tags
+        if (annotations.annotations && annotations.annotations.tags) {
+            annotations.annotations.tags.forEach((tag, index) => {
+                variableDefinitions.push(
+                    { 
+                        variableId: `annotation_track_${transportName}_tag${index + 1}_type`, 
+                        name: `Track ${annotations.name}: Tag ${index + 1} Type` 
+                    },
+                    { 
+                        variableId: `annotation_track_${transportName}_tag${index + 1}_value`, 
+                        name: `Track ${annotations.name}: Tag ${index + 1} Value` 
+                    }
+                )
+            })
+        }
     }
 
     return variableDefinitions
@@ -42,26 +48,32 @@ export async function fetchAnnotationVariableDefinitions(self, data) {
 export async function fetchAnnotationVariableValues(self, data) {
     const variableValues = {}
 
-    // Handle single annotation result
-    const annotations = data.result
-    if (!annotations) return variableValues
-
-    // Get transport name from the result and sanitize it for use in variable names
-    const trackName = annotations.name ? annotations.name.replace(/\s+/g, '_') : 'unknown'
-
-    // Set values for notes
-    if (annotations.annotations && annotations.annotations.notes) {
-        annotations.annotations.notes.forEach((note, index) => {
-            variableValues[`track_${trackName}_note${index + 1}_text`] = note.text
-        })
+    // Handle array of annotation results
+    if (!Array.isArray(data)) {
+        data = [data]
     }
 
-    // Set values for tags
-    if (annotations.annotations && annotations.annotations.tags) {
-        annotations.annotations.tags.forEach((tag, index) => {
-            variableValues[`track_${trackName}_tag${index + 1}_type`] = tag.type
-            variableValues[`track_${trackName}_tag${index + 1}_value`] = tag.value
-        })
+    for (const annotationData of data) {
+        const annotations = annotationData.result
+        if (!annotations) continue
+
+        // Get transport name from the result and sanitize it for use in variable names
+        const transportName = annotations.name ? annotations.name.replace(/\s+/g, '_') : 'unknown'
+
+        // Set values for notes
+        if (annotations.annotations && annotations.annotations.notes) {
+            annotations.annotations.notes.forEach((note, index) => {
+                variableValues[`annotation_track_${transportName}_note${index + 1}_text`] = note.text
+            })
+        }
+
+        // Set values for tags
+        if (annotations.annotations && annotations.annotations.tags) {
+            annotations.annotations.tags.forEach((tag, index) => {
+                variableValues[`annotation_track_${transportName}_tag${index + 1}_type`] = tag.type
+                variableValues[`annotation_track_${transportName}_tag${index + 1}_value`] = tag.value
+            })
+        }
     }
 
     return variableValues
