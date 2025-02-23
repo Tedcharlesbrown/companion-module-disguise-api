36cbc95907bca3d0bd72047d7e44237493c740c8
Ted Charles Brown <52015952+Tedcharlesbrown@users.noreply.github.com>
2025-02-23 10:53:03 -0600

    track variables

--- api/session/transport/annotationVariables.js
+++ api/session/transport/annotationVariables.js
@@ -1,80 +1,84 @@
 export async function fetchAnnotationVariableDefinitions(self, data) {
     const variableDefinitions = []
 
+    // Debug log the input data
+    self.log('debug', 'Annotation data for variable definitions: ' + JSON.stringify(data))
+
     // Handle array of annotation results
-    if (!Array.isArray(data)) {
-        data = [data]
-    }
+    if (data && data.result) {
+        for (const annotation of data.result) {
+            if (!annotation) continue
 
-    for (const annotationData of data) {
-        const annotations = annotationData.result
-        if (!annotations) continue
+            // Get track name and sanitize it for use in variable names
+            const trackName = annotation.name ? annotation.name.replace(/\s+/g, '_') : 'unknown'
+            self.log('debug', `Processing annotations for track: ${trackName}`)
 
-        // Get transport name from the result and sanitize it for use in variable names
-        const transportName = annotations.name ? annotations.name.replace(/\s+/g, '_') : 'unknown'
+            // Add variables for notes
+            if (annotation.annotations && annotation.annotations.notes) {
+                annotation.annotations.notes.forEach((note, index) => {
+                    variableDefinitions.push({ 
+                        variableId: `track_${trackName}_note${index + 1}_text`, 
+                        name: `Track ${annotation.name}: Note ${index + 1} Text` 
+                    })
+                })
+            }
 
-        // Add variables for notes
-        if (annotations.annotations && annotations.annotations.notes) {
-            annotations.annotations.notes.forEach((note, index) => {
-                variableDefinitions.push(
-                    { 
-                        variableId: `annotation_track_${transportName}_note${index + 1}_text`, 
-                        name: `Track ${annotations.name}: Note ${index + 1} Text` 
-                    }
-                )
-            })
-        }
-
-        // Add variables for tags
-        if (annotations.annotations && annotations.annotations.tags) {
-            annotations.annotations.tags.forEach((tag, index) => {
-                variableDefinitions.push(
-                    { 
-                        variableId: `annotation_track_${transportName}_tag${index + 1}_type`, 
-                        name: `Track ${annotations.name}: Tag ${index + 1} Type` 
-                    },
-                    { 
-                        variableId: `annotation_track_${transportName}_tag${index + 1}_value`, 
-                        name: `Track ${annotations.name}: Tag ${index + 1} Value` 
-                    }
-                )
-            })
+            // Add variables for tags
+            if (annotation.annotations && annotation.annotations.tags) {
+                annotation.annotations.tags.forEach((tag, index) => {
+                    variableDefinitions.push(
+                        { 
+                            variableId: `track_${trackName}_tag${index + 1}_type`, 
+                            name: `Track ${annotation.name}: Tag ${index + 1} Type` 
+                        },
+                        { 
+                            variableId: `track_${trackName}_tag${index + 1}_value`, 
+                            name: `Track ${annotation.name}: Tag ${index + 1} Value` 
+                        }
+                    )
+                })
+            }
         }
     }
 
+    // Debug log the output
+    self.log('debug', 'Created variable definitions: ' + JSON.stringify(variableDefinitions))
     return variableDefinitions
 }
 
 export async function fetchAnnotationVariableValues(self, data) {
     const variableValues = {}
 
+    // Debug log the input data
+    self.log('debug', 'Annotation data for variable values: ' + JSON.stringify(data))
+
     // Handle array of annotation results
-    if (!Array.isArray(data)) {
-        data = [data]
-    }
+    if (data && data.result) {
+        for (const annotation of data.result) {
+            if (!annotation) continue
 
-    for (const annotationData of data) {
-        const annotations = annotationData.result
-        if (!annotations) continue
+            // Get track name and sanitize it for use in variable names
+            const trackName = annotation.name ? annotation.name.replace(/\s+/g, '_') : 'unknown'
+            self.log('debug', `Setting values for track: ${trackName}`)
 
-        // Get transport name from the result and sanitize it for use in variable names
-        const transportName = annotations.name ? annotations.name.replace(/\s+/g, '_') : 'unknown'
+            // Set values for notes
+            if (annotation.annotations && annotation.annotations.notes) {
+                annotation.annotations.notes.forEach((note, index) => {
+                    variableValues[`track_${trackName}_note${index + 1}_text`] = note.text || ''
+                })
+            }
 
-        // Set values for notes
-        if (annotations.annotations && annotations.annotations.notes) {
-            annotations.annotations.notes.forEach((note, index) => {
-                variableValues[`annotation_track_${transportName}_note${index + 1}_text`] = note.text
-            })
-        }
-
-        // Set values for tags
-        if (annotations.annotations && annotations.annotations.tags) {
-            annotations.annotations.tags.forEach((tag, index) => {
-                variableValues[`annotation_track_${transportName}_tag${index + 1}_type`] = tag.type
-                variableValues[`annotation_track_${transportName}_tag${index + 1}_value`] = tag.value
-            })
+            // Set values for tags
+            if (annotation.annotations && annotation.annotations.tags) {
+                annotation.annotations.tags.forEach((tag, index) => {
+                    variableValues[`track_${trackName}_tag${index + 1}_type`] = tag.type || ''
+                    variableValues[`track_${trackName}_tag${index + 1}_value`] = tag.value || ''
+                })
+            }
         }
     }
 
+    // Debug log the output
+    self.log('debug', 'Created variable values: ' + JSON.stringify(variableValues))
     return variableValues
 }
--- variables.js
+++ variables.js
@@ -11,9 +11,37 @@ export async function request_api(self) {
 	self.transport_activetransport = await getRequest(`http://${self.config.ipaddress}/api/session/transport/activetransport`)
 	self.transport_tracks = await getRequest(`http://${self.config.ipaddress}/api/session/transport/tracks`)
 	
-	// Get annotations for all transports in one request
+	// Get annotations for all tracks
 	try {
-		self.transport_annotations = await getRequest(`http://${self.config.ipaddress}/api/session/transport/annotations`)
+		if (self.transport_tracks && self.transport_tracks.result) {
+			// Initialize array to store all annotation results
+			self.transport_annotations = { result: [] }
+			
+			self.log('debug', 'Fetching annotations for tracks: ' + JSON.stringify(self.transport_tracks.result))
+			
+			// Fetch annotations for each track
+			for (const track of self.transport_tracks.result) {
+				if (track.uid) {
+					try {
+						const annotationUrl = `http://${self.config.ipaddress}/api/session/transport/annotations?uid=${track.uid}`
+						self.log('debug', `Fetching annotations from: ${annotationUrl}`)
+						
+						const trackAnnotations = await getRequest(annotationUrl)
+						self.log('debug', `Received annotations for track ${track.name}: ${JSON.stringify(trackAnnotations)}`)
+						
+						if (trackAnnotations && trackAnnotations.result) {
+							// Add track name to the annotation result for reference
+							trackAnnotations.result.name = track.name
+							self.transport_annotations.result.push(trackAnnotations.result)
+						}
+					} catch (trackError) {
+						self.log('debug', `Failed to get annotations for track ${track.name}: ${trackError.message}`)
+					}
+				}
+			}
+			
+			self.log('debug', 'Final annotations object: ' + JSON.stringify(self.transport_annotations))
+		}
 	} catch (error) {
 		self.log('debug', `Failed to get annotations: ${error.message}`)
 		self.transport_annotations = { result: [] }
@@ -32,12 +60,20 @@ export async function defineVariables(self) {
 		const statusVariableDefinitions = await fetchStatusVariableDefinitions(self, [self.status_health, self.status_project]);
 		const annotationVariableDefinitions = await fetchAnnotationVariableDefinitions(self, self.transport_annotations);
 
+		// Debug log before merging
+		self.log('debug', 'Transport Variables: ' + JSON.stringify(transportVariableDefinitions));
+		self.log('debug', 'Status Variables: ' + JSON.stringify(statusVariableDefinitions));
+		self.log('debug', 'Annotation Variables: ' + JSON.stringify(annotationVariableDefinitions));
+
 		// Merge all variable definitions
 		const allVariableDefinitions = [
 			...transportVariableDefinitions,
 			...statusVariableDefinitions,
 			...annotationVariableDefinitions
 		];
+
+		// Debug log final definitions
+		self.log('debug', 'Setting variable definitions: ' + JSON.stringify(allVariableDefinitions));
 
 		// Set the merged variable definitions
 		self.setVariableDefinitions(allVariableDefinitions);
@@ -53,12 +89,20 @@ export async function updateVariables(self) {
 		const statusVariableValues = await fetchStatusVariableValues(self, [self.status_health, self.status_project]);
 		const annotationVariableValues = await fetchAnnotationVariableValues(self, self.transport_annotations);
 
+		// Debug log before merging
+		self.log('debug', 'Transport Values: ' + JSON.stringify(transportVariableValues));
+		self.log('debug', 'Status Values: ' + JSON.stringify(statusVariableValues));
+		self.log('debug', 'Annotation Values: ' + JSON.stringify(annotationVariableValues));
+
 		// Merge all variable values
 		const allVariableValues = {
 			...transportVariableValues,
 			...statusVariableValues,
 			...annotationVariableValues
 		};
+
+		// Debug log final values
+		self.log('debug', 'Setting variable values: ' + JSON.stringify(allVariableValues));
 
 		// Set the merged variable values
 		self.setVariableValues(allVariableValues);
